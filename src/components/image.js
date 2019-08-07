2b359344625869531124437a71e203879764cc20
Mike Garcia <michael.c.garcia7@gmail.com>
2019-08-07 12:30:22 -0500

    class app

---
+++ src/components/image.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './image.css';
+
+function Image(props) {
+	return (
+		<span>
+			<img src={props.src} alt={props.alt} id={props.id} key={props.id} onClick={() => props.clickme(props.id)} />
+		</span>
+	);
+}
+
+export default Image;
---
+++ src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const BASEURL = 'https://api.giphy.com/v1/gifs/search?q=';
+const APIKEY = '&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=20';
+// https://api.giphy.com/v1/gifs/search?q=&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=20
+
+export default {
+	search: function(query) {
+		return axios.get(BASEURL + query + APIKEY);
+	}
+};
